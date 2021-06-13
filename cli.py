ad109ba084417a431be5ba35ad134caec63c9079
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2021-06-13 09:16:46 +0100

    feat(make request): simple request logic, hooked with cli. working fine

--- cli.py
+++ cli.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
+import logging
+
+from src import request
+
+logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
+
+
+def make_request(url, number):
+    for i in range(number):
+        result = request.post(url)
+
+
 if __name__ == "__main__":
-    print('load generator')
+    make_request('http://localhost:8080/Live', 5)
+    logging.info('completed')
---
+++ src/request.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+
+# error https://docs.python-requests.org/en/master/user/quickstart/#timeouts
+
+# we should get requests_time from Statistics
+payload = json.dumps({
+    "name": "test",
+    "date": "09:11:00",
+    "requests_sent": 1
+})
+
+headers = {
+    'Accept': '*/*',
+    'Content-Type': 'application/json'
+}
+
+
+def post(url, timeout=2):
+    response = requests.post(url=url, data=payload, headers=headers, timeout=timeout)
+
+    if response.status_code == 200:
+        code = response.status_code
+    else:
+        code = response.status_code
+    # print(response)
+    # print(response.json())
+    return response.elapsed.microseconds
+
